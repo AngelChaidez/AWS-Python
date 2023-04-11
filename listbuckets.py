7068e265144779870251e98d844339d22ce898a1
Angel Chaidez <angelomarchaidez@gmail.com>
2023-04-11 09:37:50 -0700

    adding new files

--- listbuckets.py
+++
@@ -1,9 +0,0 @@
-# this is a test of the emergency broadcast system
-import boto3
-from botocore.exceptions import ClientError
-
-s3 = boto3.client('s3')
-response = s3.list_buckets()['Buckets']
-for bucket in response:
-    print(bucket['Name'])
-
